ab520f8d149151d83c842765f7a6afd28dce786f
Ougi <ougi@Ougis-MacBook-Pro.local>
2026-01-06 15:48:41 +0700

    fix browser router

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./pages/home";
 
 export default function App() {
   return (
-    <BrowserRouter basename="/ougi">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
