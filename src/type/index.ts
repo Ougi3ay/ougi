31716eb03df4a58c52b50036e8cd0f1b232dde22
Ougi <ougi@Ougis-MacBook-Pro.local>
2026-01-26 20:22:14 +0700

    use i18n

---
+++ src/type/index.ts
@@ -0,0 +1,25 @@
+export type SkillType = {
+    id: number;
+    name: string;
+    level: string;
+    icon: string;
+};
+
+export type ExperienceType = {
+    id: number;
+    role: string;
+    company: string;
+    description: string;
+    image?: string;
+    link?: string;
+}
+
+export type EducationType = {
+    id: number;
+    degree: string;
+    institution: string;
+    year: string;
+    description: string;
+    image?: string;
+    location?: string;
+}
